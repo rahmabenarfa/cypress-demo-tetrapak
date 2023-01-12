279d8c4c9001c8fed8c6a839e1d88541bd473267
rahmabenarfa <rahma.ben.arfa@accenture.com>
2023-01-12 15:25:38 +0100

    find username

--- cypress/e2e/test_tetrapak.cy.js
+++ cypress/e2e/test_tetrapak.cy.js
@@ -35,7 +35,7 @@
    
    //cy.get('#usernameFormGroup').find('Username or email') 
 
-   cy.get('usernameFormGroup').find('Username or email')
+   cy.find('Username or email')
 
   // cy.get('name="usernameFormGroup"').should('have.value', 'Username or email')
         
